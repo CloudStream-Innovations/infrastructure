dcff5e9dcb648d3acf461aa1a1a5bfd8e32ca4e0
Warren Byron <warren.byron@gmail.com>
2024-05-28 12:52:01 +0100

    Create s3-dag.tf

---
+++ s3-dag.tf
@@ -0,0 +1,48 @@
+#-----------------------------------------------------------
+# Create an S3 bucket and upload sample DAG
+#-----------------------------------------------------------
+#tfsec:ignore:AWS017 tfsec:ignore:AWS002 tfsec:ignore:AWS077
+resource "aws_s3_bucket" "this" {
+  bucket = local.bucket_name
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  depends_on = [aws_s3_bucket_ownership_controls.this]
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
