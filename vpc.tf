e650efebd3b8b618a1625bf514eee3b8a984977a
Warren Byron <warren.byron@gmail.com>
2024-05-28 12:55:06 +0100

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = var.name
+  cidr = var.vpc_cidr
+
+  azs             = local.azs
+  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 10)]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = var.tags
+}
