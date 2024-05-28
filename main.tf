37d7bb60c79ab588824bca9715851537f7c4fca7
Warren Byron <warren.byron@gmail.com>
2024-05-28 12:50:01 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  azs         = slice(data.aws_availability_zones.available.names, 0, 2)
+  bucket_name = format("%s-%s", "aws-ia-mwaa", data.aws_caller_identity.current.account_id)
+}
