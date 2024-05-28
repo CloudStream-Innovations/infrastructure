610305ab6d864aedcb650fb9708eb9cf66ce0044
Warren Byron <wbyron@shoprite.co.za>
2024-05-28 20:04:43 +0100

    Re-reference the dags files

--- dags.tf
+++ dags.tf
@@ -2,21 +2,21 @@
 
 # TODO : To be truly scaleable need requirements to be independent for each DAG
 # https://youtu.be/uA-8Lj1RNgw?si=LQgZ6EiYnRpcRWJY
-  
+
 # Upload DAGS
 resource "aws_s3_object" "object1" {
-  for_each = fileset("dags/dags/", "*")
+  for_each = fileset("../dags/dags/", "*")
   bucket   = aws_s3_bucket.this.id
   key      = "dags/${each.value}"
-  source   = "dags/${each.value}"
-  etag     = filemd5("dags/${each.value}")
+  source   = "../dags/dags/${each.value}"
+  etag     = filemd5("../dags/dags/${each.value}")
 }
 
 # Upload plugins/requirements.txt
 resource "aws_s3_object" "reqs" {
-  for_each = fileset("dags/mwaa/", "*")
+  for_each = fileset("../dags/mwaa/", "*")
   bucket   = aws_s3_bucket.this.id
   key      = each.value
-  source   = "mwaa/${each.value}"
-  etag     = filemd5("mwaa/${each.value}")
+  source   = "../dags/mwaa/${each.value}"
+  etag     = filemd5("../dags/mwaa/${each.value}")
 }
